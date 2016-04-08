77f07cf42584811626f40336baeaf7da1acdaef7
Miguel Coelho <miguel_coelho_1@hotmail.com>
2016-04-08 18:21:42 +0100

    Help Classes
    Classes da ajuda feitas até ao dia 08/04/2016 por Miguel Coelho nº18284

---
+++ Help Classes - 20160804/JsHelp.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function showDiv() {
+   document.getElementById('Div').style.display = "block";
+}
+function showDiv2() {
+   document.getElementById('Div2').style.display = "block";
+}
+function showDiv3() {
+   document.getElementById('Div3').style.display = "block";
+}
+
+
